5f7ac0f3c9a3b9351fd7ec35a217f3f6eab5483d
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-03 00:35:29 +0500

    update form fields in index.html with sample data and enhance data display in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // console.log(1);
 let form = document.querySelector("form");
+let div=document.querySelector("div")
 // function picSrc(input) {
 //     let files=input.files[0]
 //     return files.name
@@ -14,5 +15,10 @@ let form = document.querySelector("form");
         course: form.children[4].value,
         // pic: ""
     };
-    console.log(data);
+    div.children[1].innerText=`First Name: ${data.firstName}`
+    div.children[2].innerText=`Last Name: ${data.lastName}`
+    div.children[3].innerText=`Email: ${data.email}`
+    div.children[4].innerText=`Number: ${data.number}`
+    div.children[5].innerText=`Course: ${data.course}`
+    // console.log(data);
 }
