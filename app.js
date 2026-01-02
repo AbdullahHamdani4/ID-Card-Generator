6a9e876653fb223e57ad83583f64d5e89f7db22e
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-03 00:27:15 +0500

    refactor form handling in app.js and update index.html to ensure proper data submission

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // console.log(1);
-let form=document.querySelector("form");
-let data={
-    firstName:form.children[0].innerText,
-    lastName:form.children[1].innerText,
-    email:form.children[2].innerText,
-    number:form.children[3].innerText,
-    course:form.children[4].innerText,
-};
+let form = document.querySelector("form");
+// function picSrc(input) {
+//     let files=input.files[0]
+//     return files.name
+//  }
+ function generate(e) {
+    e.preventDefault()
+    let data = {
+        firstName: form.children[0].value,
+        lastName: form.children[1].value,
+        email: form.children[2].value,
+        number: form.children[3].value,
+        course: form.children[4].value,
+        // pic: ""
+    };
+    console.log(data);
+}
