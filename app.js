5edceb7a524ebf33212631a888c0b13f7528d7b4
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-03 00:07:27 +0500

    add data extraction logic from form in app.js and ensure submit button is present in index.html

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // console.log(1);
 let form=document.querySelector("form");
+let data={
+    firstName:form.children[0].innerText,
+    lastName:form.children[1].innerText,
+    email:form.children[2].innerText,
+    number:form.children[3].innerText,
+    course:form.children[4].innerText,
+};
