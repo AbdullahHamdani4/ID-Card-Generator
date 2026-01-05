0dfd66666de891104128f9879527851766bf4cd6
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-05 10:12:03 +0500

    refactor app.js by commenting out unused functions and variables for cleaner code

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 // console.log(1);
 let form = document.querySelector("form");
-let div=document.querySelector("div")
-  function picSrc(input) {
-    let files=input.files[0]
-    console.log(files);
-    div.children[1].src=URL.createObjectURL(files)
-    console.log(div.children[1].src);
-    return files.name
- }
- function generate(e) {
+console.log(form);
+
+// let div=document.querySelector("div")
+//   function picSrc(input) {
+//     let files=input.files[0]
+//     console.log(files);
+//     div.children[1].src=URL.createObjectURL(files)
+//     console.log(div.children[1].src);
+//     return files.name
+//  }
+//  function generate(e) {
     
-    e.preventDefault()
+//     e.preventDefault()
   
-    let data = {
-        firstName: form.children[0].value,
-        lastName: form.children[1].value,
-        email: form.children[2].value,
-        number: form.children[3].value,
-        course: form.children[4].value,
-        // pic: ""
-    };
-    div.children[1].innerText=`First Name: ${data.firstName}`
-    div.children[2].innerText=`Last Name: ${data.lastName}`
-    div.children[3].innerText=`Email: ${data.email}`
-    div.children[4].innerText=`Number: ${data.number}`
-    div.children[5].innerText=`Course: ${data.course}`
-    // console.log(data);
-}
+//     let data = {
+//         firstName: form.children[0].value,
+//         lastName: form.children[1].value,
+//         email: form.children[2].value,
+//         number: form.children[3].value,
+//         course: form.children[4].value,
+//         // pic: ""
+//     };
+//     div.children[1].innerText=`First Name: ${data.firstName}`
+//     div.children[2].innerText=`Last Name: ${data.lastName}`
+//     div.children[3].innerText=`Email: ${data.email}`
+//     div.children[4].innerText=`Number: ${data.number}`
+//     div.children[5].innerText=`Course: ${data.course}`
+//     // console.log(data);
+// }
